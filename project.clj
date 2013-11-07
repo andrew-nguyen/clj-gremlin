2546329ede2dd9d82ce6e600f2b0015ed642cc60
Andrew Nguyen <andrew-git@na-consulting.net>
2013-11-07 13:52:54 -0800

    Added support for deploying to bd-deps/snapshots s3 repository

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
-(defproject clj-gremlin "2.4.0-ALPHA"
+(defproject clj-gremlin "2.4.0-ALPHA-SNAPSHOT"
   :description "Implementation of TinkerPop Gremlin 2.0"
   :url "https://github.com/olabini/gremlin-clj"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.tinkerpop.gremlin/gremlin-java "2.4.0"]
                  [com.tinkerpop.blueprints/blueprints-core "2.4.0"]]
   :aot [clj-gremlin.pipeline]
-;;  :warn-on-reflection true
-  )
+
+;  :warn-on-reflection true
+  
+  :repositories [["bd-snapshots" {:url "s3p://bd-deps/snapshots/"
+                                  :username ~(System/getenv "LEIN_USERNAME")
+                                  :passphrase ~(System/getenv "LEIN_PASSPHRASE")}]]
+
+  :plugins [[s3-wagon-private "1.1.2"]])
